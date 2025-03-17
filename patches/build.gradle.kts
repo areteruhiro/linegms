233bfa2034a895ccc94f32a776cd433cd55f8291
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-17 19:24:38 +0900

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,37 +1,40 @@
-group = "app.revanced"
+group = "areteruhiro"
 
 patches {
     about {
-        name = "ReVanced Patches"
+        name = "a"
         description = "Patches for ReVanced"
         source = "git@github.com:revanced/revanced-patches.git"
-        author = "ReVanced"
+        author = "LIMEs"
         contact = "contact@revanced.app"
-        website = "https://revanced.app"
+        website = "https://your.homepage"
         license = "GNU General Public License v3.0"
     }
 }
 
 dependencies {
-    // Used by JsonGenerator.
-    implementation(libs.gson)
+    // Required due to smali, or build fails. Can be removed once smali is bumped.
+    implementation(libs.guava)
+    // Android API stubs defined here.
+    compileOnly(project(":patches:stub"))
 }
 
 tasks {
-    jar {
-        exclude("app/revanced/generator")
-    }
-    register<JavaExec>("generatePatchesFiles") {
-        description = "Generate patches files"
+    register<JavaExec>("preprocessCrowdinStrings") {
+        description = "Preprocess strings for Crowdin push"
 
-        dependsOn(build)
+        dependsOn(compileKotlin)
 
         classpath = sourceSets["main"].runtimeClasspath
-        mainClass.set("app.revanced.generator.MainKt")
-    }
-    // Used by gradle-semantic-release-plugin.
-    publish {
-        dependsOn("generatePatchesFiles")
+        mainClass.set("app.revanced.util.CrowdinPreprocessorKt")
+
+        args = listOf(
+            "src/main/resources/addresources/values/strings.xml",
+            // Ideally this would use build/tmp/crowdin/strings.xml
+            // But using that does not work with Crowdin pull because
+            // it does not recognize the strings.xml file belongs to this project.
+            "src/main/resources/addresources/values/strings.xml"
+        )
     }
 }
 
@@ -42,13 +45,33 @@ kotlin {
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/inotia00/revanced-patches")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+    publications {
+        create<MavenPublication>("revanced-patches-publication") {
+            from(components["java"])
+
+            pom {
+                name = "LIMEs"
+                description = "Patches for ReVanced."
+                url = "https://your.homepage"
+
+                licenses {
+                    license {
+                        name = "GNU General Public License v3.0"
+                        url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Your ID"
+                        name = "LIMEs"
+                        email = "contact@your.homepage"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/areteruhiro/revanced-patches.git"
+                    developerConnection = "scm:git:git@github.com:areteruhiro/revanced-patches.git"
+                    url = "https://github.com/crimera/piko"
+                }
             }
         }
     }
